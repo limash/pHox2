ea3f5737ffcd3bf4a488db8fbf747be69e023e01
sia <yakubov.sha@gmail.com>
2026-05-28 15:04:10 +0200

    Fix real hardware imports

--- src/phox2/hardware/real/__init__.py
+++ src/phox2/hardware/real/__init__.py
@@ -1,5 +1,5 @@
 from phox2.hardware.real.adc import ADCDifferentialPiReader
-from phox2.hardware.real.gpio import PigpioDigitalOutput
+from phox2.hardware.real.gpio import LgpioDigitalOutput
 from phox2.hardware.real.spectrometer import SeabreezeSpectrometer
 
-__all__ = ["PigpioDigitalOutput", "ADCDifferentialPiReader", "SeabreezeSpectrometer"]
+__all__ = ["LgpioDigitalOutput", "ADCDifferentialPiReader", "SeabreezeSpectrometer"]
